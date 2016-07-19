088fb9c3a344590c6a3f2de5cf8caa03d9eff4e2
Julio Camarero <julio.camarero@liferay.com>
2016-07-19 18:05:35 +0200

    Create initial view and edit pokemon view

---
+++ pokedex-web/src/main/java/com/liferay/pokedex/web/action/EditPokemonMVCRenderCommand.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.pokedex.web.action;
+
+import com.liferay.pokedex.web.portlet.PokedexPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.util.WebKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Julio Camarero
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + PokedexPortletKeys.POKEDEX,
+		"mvc.command.name=" + EditPokemonMVCRenderCommand.PATH
+	},
+	service = MVCRenderCommand.class
+)
+public class EditPokemonMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		Template template = (Template)renderRequest.getAttribute(
+			WebKeys.TEMPLATE);
+
+		PortletURL portletURL = renderResponse.createRenderURL();
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "pokedex_view");
+
+		template.put("backPortletURL", portletURL.toString());
+
+		return PATH;
+	}
+
+	public static final String PATH = "pokedex_edit_pokemon";
+
+}
---
+++ pokedex-web/src/main/java/com/liferay/pokedex/web/action/ViewMVCRenderCommand.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.pokedex.web.action;
+
+import com.liferay.pokedex.web.portlet.PokedexPortletKeys;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Julio Camarero
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + PokedexPortletKeys.POKEDEX,
+		"mvc.command.name=/", "mvc.command.name=" + ViewMVCRenderCommand.PATH
+	},
+	service = MVCRenderCommand.class
+)
+public class ViewMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		Template template = (Template)renderRequest.getAttribute(
+			WebKeys.TEMPLATE);
+
+		PortletURL portletURL = renderResponse.createRenderURL();
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "pokedex_edit_pokemon");
+
+		template.put("editPokemonPortletURL", portletURL.toString());
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		User user = themeDisplay.getUser();
+
+		template.put("userName", user.getFirstName());
+
+		return PATH;
+	}
+
+	public static final String PATH = "pokedex_view";
+
+}
