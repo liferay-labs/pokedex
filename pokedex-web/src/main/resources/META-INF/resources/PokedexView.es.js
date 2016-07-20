941d173fe9889b6e6e944fc93670d832b8cdd051
Carlos Lancha <carlos.lancha@liferay.com>
2016-07-21 00:09:54 +0200

    Add tooltip to add button

--- pokedex-web/src/main/resources/META-INF/resources/PokedexView.es.js
+++ pokedex-web/src/main/resources/META-INF/resources/PokedexView.es.js
@@ -2,6 +2,7 @@ import Component from 'metal-component/src/Component';
 import Soy from 'metal-soy/src/Soy';
 
 import Dropdown from 'metal-dropdown/src/Dropdown';
+import Tooltip from 'metal-tooltip/src/Tooltip';
 
 import templates from './PokedexView.soy';
 
