8ea821f5774b971ee2098a11d9bb613545599ce7
antoniopol06 <antonio.pol@liferay.com>
2016-07-21 14:47:35 +0200

    Metal autocomplete

--- pokedex-web/src/main/resources/META-INF/resources/PokedexView.es.js
+++ pokedex-web/src/main/resources/META-INF/resources/PokedexView.es.js
@@ -3,6 +3,7 @@ import Soy from 'metal-soy/src/Soy';
 
 import Dropdown from 'metal-dropdown/src/Dropdown';
 import Tooltip from 'metal-tooltip/src/Tooltip';
+import Autocomplete from 'metal-autocomplete/src/Autocomplete';
 
 import templates from './PokedexView.soy';
 import pokemonViewTemplates from './PokemonListItem.soy';
@@ -15,6 +16,23 @@ class PokedexView extends Component {
 		super(opt_config);
 		this.listPokemons = opt_config.pokemons.slice();
 		this.pokemons = opt_config.pokemons;
+	}
+
+	attached(){
+		var instance = this;
+
+		new Autocomplete(
+			{
+				elementClasses: 'autocomplete-pokemon',
+				inputElement: document.getElementById('input-search'),
+				data: function(query) {
+					return instance.listPokemons.filter(function(item) {
+						item.textPrimary = item.name;
+						return query && item.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+					});
+				}
+			}
+		);
 	}
 
 	filter(event) {
