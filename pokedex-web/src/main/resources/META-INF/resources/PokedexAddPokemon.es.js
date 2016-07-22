c7e781efe81d9fdf2d09a596a4c015dbead11b8e
Carlos Lancha <carlos.lancha@liferay.com>
2016-07-22 13:19:16 +0200

    Do ajax request

--- pokedex-web/src/main/resources/META-INF/resources/PokedexAddPokemon.es.js
+++ pokedex-web/src/main/resources/META-INF/resources/PokedexAddPokemon.es.js
@@ -3,6 +3,7 @@ import Soy from 'metal-soy/src/Soy';
 
 import Ajax from 'metal-ajax/src/Ajax';
 import Dropdown from 'metal-dropdown/src/Dropdown';
+import MultiMap from 'metal-multimap/src/MultiMap';
 
 import templates from './PokedexAddPokemon.soy';
 import pokemonViewTemplates from './PokemonListItem.soy';
@@ -13,9 +14,14 @@ class PokedexAddPokemon extends Component {
 	 * @inheritDoc
 	 */
 	constructor(opt_config) {
+		opt_config.originalPokemonList = opt_config.pokemons.slice();
+
 		super(opt_config);
 
 		this.getPokemonsURL = opt_config.getPokemonsURL;
+
+		this.start = 0;
+		this.end = 8;
 
 		//temporal
 		this.getPokemons();
@@ -24,14 +30,25 @@ class PokedexAddPokemon extends Component {
 	getPokemons() {
 		var instance = this;
 
+		let body = new MultiMap();
+
+		body.add(this.portletNamespace + 'start', this.start);
+		body.add(this.portletNamespace + 'end', this.end);
+
 		Ajax.request(
 			this.getPokemonsURL,
-			'GET'
+			'GET',
+			null,
+			null,
+			body
 		)
 		.then(
 			function(response) {
-				instance.pokemons = JSON.parse(response.response);
-				instance.originalPokemonList = JSON.parse(response.response);
+				instance.pokemons = instance.pokemons.concat(JSON.parse(response.response));
+				instance.originalPokemonList = instance.originalPokemonList.concat(JSON.parse(response.response));
+
+				instance.start += 8;
+				instance.end += 8;
 			}
 		);
 	}
@@ -43,9 +60,6 @@ class PokedexAddPokemon extends Component {
 
 PokedexAddPokemon.STATE = {
 	pokemons: {
-		value: []
-	},
-	originalPokemonList: {
 		value: []
 	}
 };
