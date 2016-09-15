b6b8395281baa910395e99bc4cb56ba99962a92f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-09-15 16:45:17 +0200

    Implements add method with call to Pokemon Rest WS

--- pokedex-web/src/main/java/com/liferay/pokedex/web/rest/PokemonRestUtil.java
+++ pokedex-web/src/main/java/com/liferay/pokedex/web/rest/PokemonRestUtil.java
@@ -15,6 +15,8 @@
 package com.liferay.pokedex.web.rest;
 
 import com.liferay.pokedex.model.Pokemon;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 
 import java.io.BufferedReader;
@@ -28,10 +30,54 @@ import java.net.URL;
  */
 public class PokemonRestUtil {
 
-	public static void addPokemon(Pokemon pokemon) {
+	public static void addPokemon(Pokemon pokemon) throws Exception {
+		URL url = new URL(_URL);
 
-		// TODO: Add RestAPI call
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 
+		connection.setDoOutput(true);
+		connection.setRequestMethod("PUT");
+		connection.setRequestProperty(
+			"Content-Type", "application/x-www-form-urlencoded");
+
+		String addDataURL = _getAddDataURL(pokemon);
+
+		OutputStream os = connection.getOutputStream();
+
+		os.write(addDataURL.getBytes());
+
+		os.flush();
+
+		if (_log.isDebugEnabled()) {
+			BufferedReader br = new BufferedReader(
+				new InputStreamReader((connection.getInputStream())));
+
+			String output;
+
+			_log.debug("Pokemon added:");
+
+			while ((output = br.readLine()) != null) {
+				_log.debug(output);
+			}
+		}
+
+		connection.disconnect();
 	}
+
+	private static String _getAddDataURL(Pokemon pokemon) {
+		StringBundler sb = new StringBundler();
+
+		sb.append("pokemonId=" + pokemon.getId() + "&");
+		sb.append("name=" + pokemon.getName() + "&");
+		sb.append("type=" + pokemon.getType() + "&");
+		sb.append("smallImageURL=" + pokemon.getFrontShinyImageURL());
+
+		return sb.toString();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PokemonRestUtil.class);
+
+	private static final String _URL = "http://localhost:8070/pokemon/add";
 
 }
